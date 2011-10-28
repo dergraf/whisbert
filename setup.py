ff1f8a7fc63335aeffbb5596a7888688c94b7810
Andre Graf <andre@dergraf.org>
2011-10-28 14:30:32 +0200

    corrected dependencies

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name = 'whisbert',
         'python-bert',
         'python-ernie'],
     dependency_links = [
-        'https://github.com/samuel/python-bert/tarball/master#egg=bert',
-        'https://github.com/tylerneylon/python-ernie/tarball/master#egg=ernie'
+        'http://github.com/samuel/python-bert/tarball/master#egg=bert-2.0.0',
+        'http://github.com/tylerneylon/python-ernie/tarball/master#egg=ernie-2.0.0'
     ]
 )
